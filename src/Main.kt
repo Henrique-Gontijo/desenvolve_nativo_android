e439032818908ea3584e9a1e50f7f3f489168eee
Henrique-Gontijo <henriquegontijo1507@gmail.com>
2026-02-09 15:41:24 -0300

    Aula 4 - Operadores e expressões

--- src/Main.kt
+++ src/Main.kt
@@ -1,34 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val nome = "Maria" // `val` - Não pode ser alterado depois
-    var idade = 25 // `var` - Pode ser alterado
-    println("Nome: $nome, Idade: $idade")
-    idade = 26 // Permitido, pois `idade` é um `var`
-    println("Nova idade: $idade")
-    // nome = "João" // ❌ Erro! `val` não pode ser modificado
-
-    println("\n------------------------------------------------------------\n")
-
-    val numeroInt: Int = 10
-    val numeroDouble: Double = 5.5
-    val condicao: Boolean = true
-    val texto: String = "Aprendendo Kotlin!"
-    println("Numero inteiro: $numeroInt")
-    println("Numero decimal: $numeroDouble")
-    println("Valor booleano: $condicao")
-    println("Texto: $texto")
-
-    println("\n------------------------------------------------------------\n")
-
-    print("Digite seu nome: ")
-    val name = readLine() // Lê um texto digitado pelo usuário
-    print("Digite sua idade: ")
-    val old = readLine()?.toIntOrNull() // Converte para `Int`, evitando erro caso o usuário digite algo inválido
-    println("Olá, $name! Você tem $old anos.")
-
-    println("\n------------------------------------------------------------\n")
-
     val a = 10
     val b = 3
     println("Soma: ${a + b}") // 13
@@ -36,4 +8,33 @@ fun main() {
     println("Multiplicação: ${a * b}") // 30
     println("Divisão: ${a / b}") // 3 (Inteiro)
     println("Módulo: ${a % b}") // 1 (Resto da divisão)
+
+    println("\n------------------------------------------------------------\n")
+
+    val idade = 25
+    val temCarteira = true
+    println("Pode dirigir? ${(idade >= 18) && temCarteira}") // true
+    println("Precisa renovar CNH? ${(idade >= 18) || temCarteira}") // true
+    println("Não tem carteira: ${!temCarteira}") // false
+
+
+
+    println("\n------------------------------------------------------------\n")
+
+    val resultado = 10 + 5 * 2 // Multiplicação tem prioridade
+    println(resultado) // 20
+    val resultadoCorreto = (10 + 5) * 2 // Parênteses alteram a ordem
+    println(resultadoCorreto) // 30
+
+    println("\n------------------------------------------------------------\n")
+
+    val x = 10
+    val y = 5
+    val z = 2
+    val expressao1 = x > y && y > z // true && true → true
+    val expressao2 = x > y || x < z // true || false → true
+    val expressao3 = !(x < y) // !(false) → true
+    println("Expressão 1: $expressao1") // true
+    println("Expressão 2: $expressao2") // true
+    println("Expressão 3: $expressao3") // true
 }
