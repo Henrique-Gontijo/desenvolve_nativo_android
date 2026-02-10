5a6ef286f0184969aee21731a86aee9b78f2ad7f
Henrique-Gontijo <henriquegontijo1507@gmail.com>
2026-02-10 15:11:39 -0300

    Aula 8 - Listas e Arrays

--- src/Main.kt
+++ src/Main.kt
@@ -1,52 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun saudacao1() {
-    println("Olá, bem-vindo ao Kotlin!")
-}
-
-fun saudacao2(nome: String) {
-    println("Olá, $nome! Seja bem-vindo.")
-}
-
-fun somar(a: Int, b: Int): Int {
-    return a + b
-}
-
-inline fun mensagem() {
-    println("Esta é uma função inline!")
-}
-fun operar(numero: Int, operacao: (Int) -> Int): Int {
-    return operacao(numero)
-}
-
 fun main() {
-    saudacao1()
+    val numeros1 = arrayOf(10, 20, 30, 40, 50)
+
+    println(numeros1[0]) // Primeiro elemento
+    println(numeros1[2]) // Terceiro elemento
 
     println("\n------------------------------------------------------------\n")
 
-    saudacao2("Maria")
-    saudacao2("João")
+    val frutas1 = listOf("Maçã", "Banana", "Uva")
+
+    println(frutas1[0]) // Acessa o primeiro elemento
+    println(frutas1.size) // Tamanho da lista
+
+    println("\n------------------------------------------------------------\n")
+
+    val numeros2 = mutableListOf(1, 2, 3)
+
+    numeros2.add(4) // Adiciona um elemento
+    numeros2.removeAt(1) // Remove o segundo elemento
+    numeros2[0] = 10 // Modifica o primeiro elemento
+    println(numeros2) // Saída: [10, 3, 4]
 
 
     println("\n------------------------------------------------------------\n")
 
-    val resultado1 = somar(10, 5)
-    println("Resultado da soma: $resultado1")
+    val frutas2 = listOf("Maçã", "Banana", "Uva")
+
+    for (fruta in frutas2) {
+        println(fruta)
+    }
 
     println("\n------------------------------------------------------------\n")
 
-    mensagem()
+    val numeros3 = listOf(1, 2, 3, 4)
+
+    numeros3.forEach { numero ->
+        println("Número: $numero")
+    }
 
     println("\n------------------------------------------------------------\n")
 
-    //Exemplo de Expressão Lambda
-    val dobrar: (Int) -> Int = { numero -> numero * 2 }
-    println("O dobro de 5 é: ${dobrar(5)}")
+    val numeros4 = listOf(10, 20, 30)
 
+    var i = 0
+    while (i < numeros4.size) {
+        println("Número: ${numeros4[i]}")
+        i++
+    }
 
-    println("\n------------------------------------------------------------\n")
-
-    val resultado2 = operar(4) { it * 3 }
-    println("O resultado é: $resultado2")
 
 }
